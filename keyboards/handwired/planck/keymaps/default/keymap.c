257941b1fd8d4d1452baf580a3681e566ec2d795
Korachal Phadvibulya <boatmef@gmail.com>
2019-03-08 21:40:11 +0800

    modified keymaps

--- keyboards/handwired/planck/keymaps/default/keymap.c
+++ keyboards/handwired/planck/keymaps/default/keymap.c
@@ -22,6 +22,7 @@ extern keymap_config_t keymap_config;
 enum planck_layers {
   _QWERTY,
   _RAISE,
+  _RAISE2,
   _NUMBER,
   _NAV,
   _FN,
@@ -33,12 +34,14 @@ enum planck_keycodes {
 };
 
 #define RAISE MO(_RAISE)
+#define RAISE2 MO(_RAISE2)
 #define NUMBER MO(_NUMBER)
 #define NAV_CLN LT(_NAV, KC_SCLN)
-#define FN MO(_FN)
+#define FN_ENT LT(_FN, KC_ENT)
 #define MOUSE MO(_MOUSE)
 #define CTL_ESC LCTL_T(KC_ESC)
 #define SFT_ENT RSFT_T(KC_ENT)
+#define RAISE2_ENT LT(_RAISE2, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -48,16 +51,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        |------+------+------+------+------+------|------+------+------+------+------+------|
  *    CTL | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ; NAV|  '   |
  *        |------+------+------+------+------+------|------+------+------+------+------+------|
- *        | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter | Shift
+ *        | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  *        |------+------+------+------+------+------+------+------+------+------+------+------|
- *        | NUM  | Ctrl | Alt  | GUI  |RAISE |    Space    |RAISE |  FN  |      |      |MOUSE |
+ *        | NUM  | Ctrl | Alt  | GUI  |RAISE |    Space    |RAISE2|FN_ENT|      |      |MOUSE |
  *        `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R   , KC_T    , KC_Y    , KC_U   , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     CTL_ESC  , KC_A     , KC_S     , KC_D     , KC_F   , KC_G    , KC_H    , KC_J   , KC_K     , KC_L     , NAV_CLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V   , KC_B    , KC_N    , KC_M   , KC_COMM  , KC_DOT   , KC_SLSH  , SFT_ENT  ,
-    NUMBER   , KC_LCTL  , KC_LALT  , KC_LGUI  , RAISE  , KC_SPC  , KC_SPC  , RAISE  , FN       , XXXXXXX  , XXXXXXX  , MOUSE
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V   , KC_B    , KC_N    , KC_M   , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
+    NUMBER   , KC_LCTL  , KC_LALT  , KC_LGUI  , RAISE  , KC_SPC  , KC_SPC  , RAISE2 , FN_ENT   , XXXXXXX  , XXXXXXX  , MOUSE
 ),
 
 /* RAISE
@@ -66,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        |------+------+------+------+------+------|------+------+------+------+------+------|
  *        |      |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  \   |
  *        |------+------+------+------+------+------|------+------+------+------+------+------|
- *        |      |      |      |      |      |      |      |  -   |  =   |  [   |  ]   |      |
+ *        |      |  _   |  +   |  {   |  }   |      |      |  -   |  =   |  [   |  ]   |      |
  *        |------+------+------+------+------+------+------+------+------+------+------+------|
  *        |      |      |      |      |      |             |      |      |      |      |      |
  *        `-----------------------------------------------------------------------------------'
@@ -74,7 +77,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV  , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_DEL  ,
     _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS ,
-    _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , _______ ,
+    _______ , KC_UNDS , KC_PLUS , KC_LCBR , KC_RCBR , _______ , _______ , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
+),
+
+/* RAISE2
+ *        ,-----------------------------------------------------------------------------------.
+ *        |  `   |  !   |  @   |  #   |  $   |  %   |  ^   |  &   |  *   |  (   |  )   | Del  |
+ *        |------+------+------+------+------+------|------+------+------+------+------+------|
+ *        |      |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  \   |
+ *        |------+------+------+------+------+------|------+------+------+------+------+------|
+ *        |      |  -   |  =   |  [   |  ]   |      |      |  _   |  +   |  {   |  }   |      |
+ *        |------+------+------+------+------+------+------+------+------+------+------+------|
+ *        |      |      |      |      |      |             |      |      |      |      |      |
+ *        `-----------------------------------------------------------------------------------'
+ */
+[_RAISE2] = LAYOUT_planck_grid(
+    KC_GRV  , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_DEL  ,
+    _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS ,
+    _______ , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , _______ , _______ , KC_UNDS , KC_PLUS , KC_LCBR , KC_RCBR , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
 ),
 

0f3546312b53ffb6c482d0e4960fc618233c15a2
Korachal Phadvibulya <korachal@go.exchange>
2020-01-22 22:30:55 +0700

    update

--- keyboards/salmonsushi/blanck/keymaps/default/keymap.c
+++ keyboards/salmonsushi/blanck/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R  , KC_T    , KC_Y    , KC_U   , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     CTL_ESC  , KC_A     , KC_S     , KC_D     , KC_F  , KC_G    , KC_H    , KC_J   , KC_K     , KC_L     , NAV_CLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V  , KC_B    , KC_N    , KC_M   , KC_COMM  , KC_DOT   , KC_SLSH  , SFT_ENT  ,
-    NUMBER   , KC_LCTL  , KC_LALT  , KC_LCMD  , POW   , KC_SPC  , KC_SPC  , LOWER  , FN       , XXXXXXX  , DYN      , POW
+    NUMBER   , KC_LCTL  , KC_LALT  , KC_LCMD  , POW   , KC_SPC  , KC_SPC  , LOWER  , FN       , XXXXXXX  , DYN      , NAV
 ),
 
 [_LOWER] = LAYOUT(

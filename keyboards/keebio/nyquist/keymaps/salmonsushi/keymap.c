4e7db2a5c9ad7f196567277432b4668a2a73b51d
Korachal Phadvibulya <korachalph@gmail.com>
2020-05-17 12:06:36 +0700

    update keymap

--- keyboards/keebio/nyquist/keymaps/salmonsushi/keymap.c
+++ keyboards/keebio/nyquist/keymaps/salmonsushi/keymap.c
@@ -23,6 +23,7 @@ enum salmonsushi_keycodes {
 #define DYN MO(_DYN)
 
 #define CTL_ESC LCTL_T(KC_ESC)
+#define CTL_GRV LCTL_T(KC_GRV)
 #define SFT_ENT RSFT_T(KC_ENT)
 #define PRVTAB SGUI(KC_LBRC)
 #define NXTTAB SGUI(KC_RBRC)
@@ -44,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______  ,
     KC_TILD  , KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  , KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_MINS  , KC_EQL   , _______  ,
-    KC_GRV   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSLS  ,
+    CTL_GRV  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSLS  ,
     _______  , KC_LCBR  , KC_RCBR  , KC_LPRN  , KC_RPRN  , KC_TILD  , KC_PDOT  , KC_UNDS  , KC_PLUS  , KC_LBRC  , KC_RBRC  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______
 ),
@@ -67,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FN] = LAYOUT(
     _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , KC_F12   ,
+    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______  ,
+    _______  , KC_F11   , KC_F12   , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , KC_UP    , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT
 ),
@@ -83,10 +84,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUMBER] = LAYOUT(
     _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , KC_UP    , _______  , _______  , KC_PERC  , KC_KP_7  , KC_KP_8  , KC_KP_9  , KC_PAST  , _______  ,
+    _______  , _______  , _______  , KC_UP    , _______  , _______  , KC_PERC  , KC_KP_7  , KC_KP_8  , KC_KP_9  , KC_PAST  , KC_DEL   ,
     _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , KC_LPRN  , KC_KP_4  , KC_KP_5  , KC_KP_6  , KC_PPLS  , KC_PEQL  ,
     _______  , _______  , _______  , _______  , _______  , _______  , KC_RPRN  , KC_KP_1  , KC_KP_2  , KC_KP_3  , KC_PMNS  , _______  ,
-    _______  , _______  , _______  , _______  , KC_LOCK  , _______  , _______  , KC_KP_0  , XXXXXXX  , KC_PDOT  , KC_PSLS  , _______
+    _______  , _______  , _______  , _______  , KC_LOCK  , _______  , _______  , KC_KP_0  , KC_PDOT  , XXXXXXX  , KC_PSLS  , _______
 ),
 
 };
